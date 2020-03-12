9188aa31c25010fe87b81809d93b4c6ce193e19a
amrin <amrin.wan@gmail.com>
2020-03-12 23:13:50 +0800

    added main page

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -6,13 +6,87 @@ class MainScreen extends StatefulWidget {
 }
 
 class _MainScreenState extends State<MainScreen> {
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: false,
+      key: _scaffoldKey,
       appBar: AppBar(
-        title: Text('Daily Diary'),
+        leading: IconButton(
+          icon: Icon(Icons.account_circle),
+          onPressed: () {
+            _scaffoldKey.currentState.openDrawer();
+          },
+        ),
+        title: Text('Journal'),
+        actions: <Widget>[
+          IconButton(icon: Icon(Icons.more_vert), onPressed: () {}),
+        ],
       ),
-      body: Container(),
+      body: mainBody(),
+    );
+  }
+
+  Widget mainBody() {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+      child: Column(
+        children: <Widget>[
+          Container(
+            padding: EdgeInsets.only(top: 10),
+            // TextField need to be changed as Background goes 
+            // square when focused
+            child: TextField(
+              decoration: InputDecoration(
+                fillColor: Colors.white,
+                filled: true,
+                labelText: 'Search Entries',
+                suffixIcon: Icon(Icons.search),
+                enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(20),
+                  borderSide: BorderSide(color: Colors.white, width: 0),
+                ),
+              ),
+            ),
+          ),
+          SizedBox(height: 25),
+          Container(
+            padding: EdgeInsets.only(left: 10),
+            alignment: Alignment.centerLeft,
+            child: Text(
+              'Notebooks',
+              style: TextStyle(fontSize: 24),
+            ),
+          ),
+          SizedBox(height: 25),
+          Container(
+            child: Column(
+              children: <Widget>[
+                customCard('November 14', 'Event'),
+                customCard('November 17', 'Another Event'),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget customCard(String subject, String body) {
+    return Card(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: ListTile(
+        title: Text(
+          subject,
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        subtitle: Text(body),
+      ),
     );
   }
 }
